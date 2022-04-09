640fff2e57d14dfe55d78aca4861b6c5f177fe65
Juan <juansued19@gmail.com>
2022-04-09 19:49:52 -0300

    versão com as cartas espelhadas

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,77 @@
 let numeroDeCartas;
 let condicao = true;
-while (condicao) {
-  numeroDeCartas = Number(prompt("Com quantas cartas deseja jogar?"));
+let cartas;
+let imgCount = 0;
+const cardList = [];
 
-  if (numeroDeCartas % 2 === 0 && numeroDeCartas >= 4 && numeroDeCartas <= 14) {
-    alert(`${numeroDeCartas} é um numero válido`);
-    arrumarCartas();
-    condicao = false;
+const imagensCartas = [
+  "revertitparrot.gif",
+  "tripletsparrot.gif",
+  "bobrossparrot.gif",
+  "metalparrot.gif",
+  "explodyparrot.gif",
+  "fiestaparrot.gif",
+  "unicornparrot.gif",
+];
+
+//// begin
+qtdDeCartas();
+
+// pega a quantidade de cartas
+function qtdDeCartas() {
+  while (condicao) {
+    numeroDeCartas = Number(prompt("Com quantas cartas deseja jogar?"));
+
+    if (
+      numeroDeCartas % 2 === 0 &&
+      numeroDeCartas >= 4 &&
+      numeroDeCartas <= 14
+    ) {
+      arrumarCartas();
+      condicao = false;
+    } else {
+      alert("Escolha um número par entre 4 e 14!");
+    }
   }
 }
 
+// adiciona uma quantidade de cartas dentro da array cardList
 function arrumarCartas() {
-  alert("entrou na função");
   console.log(numeroDeCartas);
-  const cartas = document.querySelector(".listCards");
-  for (i = 0; i < numeroDeCartas; i++) {
-    cartas.innerHTML +=
-      "<div class='card'><img src='img/front.png' alt='' /></div>";
+
+  for (let i = 0; i < numeroDeCartas; i++) {
+    cardList.push(
+      `<div class='card'/>
+      <div class='face front'> <img src='img/front.png' alt='' /></div>
+      <div class='face back'"> <img src='img/${imagensCartas[imgCount]}' alt=''/></div>
+     
+      </div>`
+    );
+    if (i % 2 === 1) {
+      imgCount++;
+    }
+  }
+  embaralhar();
+}
+
+function embaralhar() {
+  cartasAleatorias = cardList.sort(comparador);
+  cartas = document.querySelector(".listCards");
+  console.log(cartas);
+
+  for (let i = 0; i < numeroDeCartas; i++) {
+    cartas.innerHTML += cartasAleatorias[i];
+    console.log(cardList);
+  }
+
+  function comparador() {
+    return Math.random() - 0.5;
+  }
+
+  function virarCarta(elemento) {
+    elemento.classList.toggle("flip");
   }
 }
+// adicionar na array
+//embaralhar a array
+// pegar as cartas da array
