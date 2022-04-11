c9f85a60fa23d8e9c1bcd373a6665f267bfa57c5
Juan <juansued19@gmail.com>
2022-04-11 04:06:40 -0300

    versão nova

--- js/scripts.js
+++ js/scripts.js
@@ -25,10 +25,41 @@ function addCards() {
   for (let i = 0; i < chosenCards.length; i++) {
     ul.innerHTML += `
     <ul class="card" onClick="turnSelectedCard(this)">
-    <li class="face"></li>
+    <li class="face">
+    <img src="img/front.png">
+    </li>
     <li class="back-face back img"><img src="${chosenCards[i]}"></li>
     </ul>`;
   }
 }
 
 addCards();
+function turnSelectedCard(element) {
+  let cardSelecionadoBack = document.querySelector(".selectedBackFace");
+  let cardSelecionadoFront = document.querySelector(".selectedFrontFace");
+  element.querySelector(".face").classList.add("selectedFrontFace");
+  element.querySelector(".back-face").classList.add("selectedBackFace");
+
+  if (cardSelecionadoBack !== null) {
+    if (
+      element.querySelector(".back-face").innerHTML ===
+      cardSelecionadoBack.innerHTML
+    ) {
+      element.querySelector(".face").classList.add("matchFrontFace");
+      element.querySelector(".back-face").classList.add("matchBackFace");
+      cardSelecionadoFront.classList.add("matchFrontFace");
+      cardSelecionadoBack.classList.add("matchBackFace");
+      matches++;
+    }
+    setTimeout(function () {
+      element.querySelector(".face").classList.remove("selectedFrontFace");
+      element.querySelector(".back-face").classList.remove("selectedBackFace");
+      document
+        .querySelector(".selectedBackFace")
+        .classList.remove("selectedBackFace");
+      document
+        .querySelector(".selectedFrontFace")
+        .classList.remove("selectedFrontFace");
+    }, 1000);
+  }
+}
